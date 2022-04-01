b566eecd8c494e8098f34d010deed6a540eace0a
Annabis <Ayakpeople@gmail.com>
2022-04-01 12:42:27 +0300

    done

--- Занятие1/Практические_задания/task2_1/main.py
+++ Занятие1/Практические_задания/task2_1/main.py
@@ -1,3 +1,4 @@
-input_str = input()  # TODO заполните подсказку для функции input
+input_str = input("Введите строку Hello World: ")  # TODO заполните подсказку для функции input
 
 # TODO c помощью функции print рачпечатайте значение переменной input_str и её тип
+print(input_str, type (input_str))
